c5c4636e5e894490192c2dc74d0f1b01c244c551
20k <icedshot@gmail.com>
2026-06-29 02:39:29 +0100

    more nabbing their code bits

--- backends/imgui_impl_godot_renderer.cpp
+++ backends/imgui_impl_godot_renderer.cpp
@@ -78,6 +78,14 @@ struct ImGui_ImplGodotRenderer_Data* ImGui_ImplGodotRenderer_GetBackendData()
     return ImGui::GetCurrentContext() ? (ImGui_ImplGodotRenderer_Data*)ImGui::GetIO().BackendRendererUserData : nullptr;
 };
 
+RID make_rid(int64_t id)
+{
+    static_assert(sizeof(id) == sizeof(RID));
+
+    RID rv;
+    memcpy(rv._native_ptr(), &id, sizeof(id));
+    return rv;
+}
 
 RID get_framebuffer(RID vprid)
 {
@@ -103,3 +111,67 @@ RID get_framebuffer(RID vprid)
     bd->framebuffers[vprid] = fb;
     return fb;
 }
+
+
+void setup_buffers(ImDrawData* drawData)
+{
+    RenderingDevice* RD = RenderingServer::get_singleton()->get_rendering_device();
+
+    int globalIdxOffset = 0;
+    int globalVtxOffset = 0;
+
+    const int idxBufSize = drawData->TotalIdxCount * sizeof(ImDrawIdx);
+    godot::PackedByteArray idxBuf;
+    idxBuf.resize(idxBufSize);
+
+    const int vertBufSize = drawData->TotalVtxCount * sizeof(ImDrawVert);
+    godot::PackedByteArray vertBuf;
+    vertBuf.resize(vertBufSize);
+
+    ImGui_ImplGodotRenderer_Data* bd = ImGui_ImplGodotRenderer_GetBackendData();
+
+    for(int i = 0; i < drawData->CmdListsCount; i++)
+    {
+        ImDrawList* cmdList = drawData->CmdLists[i];
+
+        std::copy(cmdList->VtxBuffer.begin(),
+                  cmdList->VtxBuffer.end(),
+                  reinterpret_cast<ImDrawVert*>(vertBuf.ptrw() + globalVtxOffset));
+        globalVtxOffset += cmdList->VtxBuffer.size_in_bytes();
+
+        std::copy(cmdList->IdxBuffer.begin(),
+                  cmdList->IdxBuffer.end(),
+                  reinterpret_cast<ImDrawIdx*>(idxBuf.ptrw() + globalIdxOffset));
+        globalIdxOffset += cmdList->IdxBuffer.size_in_bytes();
+
+        // create a uniform set for each texture
+        for(int cmdi = 0; cmdi < cmdList->CmdBuffer.Size; ++cmdi)
+        {
+            const ImDrawCmd& drawCmd = cmdList->CmdBuffer[cmdi];
+            const ImTextureID texid = drawCmd.GetTexID();
+            if(!texid)
+                continue;
+            const RID texrid = make_rid(texid);
+            if(!RD->texture_is_valid(texrid))
+                continue;
+
+            bd->usedTextures.insert(texid);
+            if(!bd->uniformSets.contains(texid))
+            {
+                Ref<RDUniform> uniform;
+                uniform.instantiate();
+                uniform->set_binding(0);
+                uniform->set_uniform_type(RenderingDevice::UNIFORM_TYPE_SAMPLER_WITH_TEXTURE);
+                uniform->add_id(bd->sampler);
+                uniform->add_id(texrid);
+
+                bd->uniforms[0] = uniform;
+                bd->uniformSets[texid] = RD->uniform_set_create(bd->uniforms, bd->shader, 0);
+            }
+        }
+    }
+
+    RD->buffer_update(bd->idxBuffer, 0, idxBuf.size(), idxBuf);
+    RD->buffer_update(bd->vtxBuffer, 0, vertBuf.size(), vertBuf);
+}
+
